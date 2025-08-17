58f358d6c48784c86622633f82e3d156b126b55b
Narayan Poudel <poudelnarayan434@gmail.com>
2025-08-17 13:24:45 +0545

    Rebase Prisma migrations to PostgreSQL

--- prisma/migrations/20250810222222_add_email_verification/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN "emailVerified" DATETIME;
-ALTER TABLE "users" ADD COLUMN "verificationToken" TEXT;
-ALTER TABLE "users" ADD COLUMN "verificationTokenExpires" DATETIME;
