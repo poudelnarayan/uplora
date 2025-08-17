9efe1824475b3c5907d2828df340e32cbc7616d5
Narayan Poudel <39937902+poudelnarayan@users.noreply.github.com>
2025-08-18 00:04:53 +0545

    Rename next.config.ts to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   eslint: {
     // Allow production builds to successfully complete even if
