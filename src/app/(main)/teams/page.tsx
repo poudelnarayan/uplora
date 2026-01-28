916d4189e8275838928682f0355691884a23a603
Narayan Poudel <poudelnarayan434@gmail.com>
2026-01-28 10:38:37 +0545

    team redesign

--- src/app/(main)/teams/page.tsx
+++ src/app/(main)/teams/page.tsx
@@ -331,8 +331,8 @@ const Teams = () => {
                       relative grid gap-6
                       grid-cols-1
                       sm:grid-cols-2
-                      lg:grid-cols-3
-                      xl:grid-cols-4
+                      lg:grid-cols-2
+                      xl:grid-cols-2
                     ">
                       {createdTeams.map((team, index) => (
                         <TeamCard
